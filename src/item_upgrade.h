e00a9f2ba923bb4019c3a78dae66523114859049
Echo3887 <62817700+Echo3887@users.noreply.github.com>
2026-09-07 19:53:41 +0200

    Add StatUpgradeResult enum for item upgrades

--- src/item_upgrade.h
+++ src/item_upgrade.h
@@ -276,6 +276,26 @@ public:
         Item* item,
         uint16 rank);
 
+    enum class StatUpgradeResult : uint8
+    {
+        Success = 0,
+        InvalidPlayer = 1,
+        InvalidItem = 2,
+        ItemNotOwned = 3,
+        InvalidStat = 4,
+        InvalidRank = 5,
+        RankNotHigher = 6,
+        StatNotPresent = 7,
+        StatNotAllowed = 8,
+        DatabaseError = 9
+    };
+    
+    StatUpgradeResult SetItemStatUpgrade(
+        Player* player,
+        Item* item,
+        uint32 statType,
+        uint16 rank);
+
     bool ChooseRandomUpgrade(Player* player, Item* item);
 
     void BuildWeaponUpgradeReqs();
